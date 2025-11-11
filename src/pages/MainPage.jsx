12b733389aff9fb64bffb0e9668849ac3e94fd32
Yooaknow <dbdkfma1228@naver.com>
2025-11-12 03:32:08 +0900

    style: 로그인/회원가입 버튼 위치 및 UI 정렬 조정

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import bgUrl from "../assets/images/main-background.svg";
 import titleImg from "../assets/images/main-title.svg";
 import signboardImg from "../assets/images/main-signboard.svg";
+import joinImg from "../assets/images/main-join.svg";
+import loginImg from "../assets/images/main-login.svg";
 
 export default function MainPage() {
   return (
@@ -12,8 +14,8 @@ export default function MainPage() {
             0% { opacity: 0; transform: translateX(-50%) translateY(20px); }
             100% { opacity: 1; transform: translateX(-50%) translateY(0); }
           }
-
-          .title-fade {
+          .fade-item {
+            opacity: 0;
             animation: fadeIn 1.2s ease-out forwards;
           }
         `}
@@ -24,11 +26,10 @@ export default function MainPage() {
           position: "fixed",
           inset: 0,
           background: "white",
-          overflow: "hidden",
           display: "flex",
           alignItems: "center",
           justifyContent: "center",
-          padding: "2rem"
+          padding: "2rem",
         }}
       >
         <div
@@ -36,70 +37,157 @@ export default function MainPage() {
             position: "relative",
             width: "1200px",
             height: "675px",
-            maxWidth: "calc(100vw - 4rem)",
-            maxHeight: "calc(100vh - 4rem)",
-            aspectRatio: "16/9",
             background: "black",
-            overflow: "hidden"
+            overflow: "hidden",
           }}
         >
           <img
             src={bgUrl}
             alt="background"
-            draggable={false}
             style={{
               position: "absolute",
               inset: 0,
               width: "100%",
               height: "100%",
               objectFit: "cover",
-              pointerEvents: "none",
-              userSelect: "none",
-              zIndex: 0
             }}
           />
 
-          {}
           <img
             src={titleImg}
             alt="title"
-            draggable={false}
-            className="title-fade"
+            className="fade-item"
             style={{
               position: "absolute",
-              top: "140px",
+              top: "155px",
               left: "50%",
               transform: "translateX(-50%)",
               width: "700px",
-              objectFit: "contain",
-              pointerEvents: "none",
-              userSelect: "none",
-              opacity: 0,
-              zIndex: 9999
+              zIndex: 10,
             }}
           />
 
-          {}
-          <img
-            src={signboardImg}
-            alt="signboard"
-            draggable={false}
-            className="title-fade"
+          <div
+            className="fade-item"
             style={{
               position: "absolute",
-              top: "380px", 
+              top: "60%",
               left: "50%",
-              transform: "translateX(-50%)",
+              transform: "translate(-50%, -50%)",
               width: "500px",
-              objectFit: "contain",
-              opacity: 0,
-              pointerEvents: "none",
-              userSelect: "none",
-              zIndex: 9999
+              height: "250px",
+              backgroundImage: `url(${signboardImg})`,
+              backgroundSize: "contain",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center",
+              zIndex: 20,
             }}
-          />
+          >
+            <div
+              style={{
+                position: "relative",
+                width: "100%",
+                height: "100%",
+              }}
+            >
+              <div
+                style={{
+                  position: "absolute",
+                  left: "48%",
+                  transform: "translateX(-50%)",
+                  top: 60,
+                  width: "67%",
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "12px",
+                  fontFamily: "Giants-Regular",
+                }}
+              >
+                <div style={{ display: "flex", alignItems: "center", gap: "14px" }}>
+                  <label
+                    style={{
+                      width: "70px",
+                      fontSize: "17px",
+                      color: "#5A2E00",
+                      fontFamily: "Giants-Regular",
+                    }}
+                  >
+                    아이디
+                  </label>
+                  <input
+                    type="text"
+                    style={{
+                      flex: 1,
+                      height: "34px",
+                      background: "#E5D3B8",
+                      border: "none",
+                      borderRadius: "6px",
+                      padding: "0 8px",
+                      fontSize: "15px",
+                      outline: "none",
+                      fontFamily: "Giants-Regular",
+                    }}
+                  />
+                </div>
 
-          <div style={{ position: "relative", width: "100%", height: "100%" }} />
+                <div style={{ display: "flex", alignItems: "center", gap: "14px" }}>
+                  <label
+                    style={{
+                      width: "70px",
+                      fontSize: "17px",
+                      color: "#5A2E00",
+                      fontFamily: "Giants-Regular",
+                    }}
+                  >
+                    비밀번호
+                  </label>
+                  <input
+                    type="password"
+                    style={{
+                      flex: 1,
+                      height: "34px",
+                      background: "#E5D3B8",
+                      border: "none",
+                      borderRadius: "6px",
+                      padding: "0 8px",
+                      fontSize: "15px",
+                      outline: "none",
+                      fontFamily: "Giants-Regular",
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div
+                style={{
+                  position: "absolute",
+                  left: 90,
+                  bottom: 40,
+                  display: "flex",
+                  gap: "30px",
+                }}
+              >
+                <img
+                  src={loginImg}
+                  alt="login"
+                  style={{
+                    width: "140px",
+                    cursor: "pointer",
+                    userSelect: "none",
+                  }}
+                />
+                <img
+                  src={joinImg}
+                  alt="join"
+                  style={{
+                    width: "140px",
+                    cursor: "pointer",
+                    userSelect: "none",
+                  }}
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
