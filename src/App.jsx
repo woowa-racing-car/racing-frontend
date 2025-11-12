2c33713d3f5ff90ad6a5c55268dffee9bda0eef2
Yooaknow <dbdkfma1228@naver.com>
2025-11-13 03:40:43 +0900

    feat(app): MyPage 라우팅 및 구조 반영

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
-// src/App.js
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainPage from "./pages/MainPage";
+import MyPage from "./pages/mypage/MyPage";
 
 export default function App() {
-  return <MainPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/mypage" element={<MyPage />} />
+      </Routes>
+    </Router>
+  );
 }
