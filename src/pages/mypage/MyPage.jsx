52e6582d56be03711c6d258afe42e860a16a47fb
Yooaknow <dbdkfma1228@naver.com>
2025-11-14 02:09:44 +0900

    feat: 마이페이지 자동차 선택 및 화면 구성 개선

--- src/pages/mypage/MyPage.jsx
+++ src/pages/mypage/MyPage.jsx
@@ -7,36 +7,60 @@ import MyPageCarInfoBoard from "./MyPageCarInfoBoard";
 
 import CommonHeader from "../../components/CommonHeader";
 
-// 자동차 이미지 import
 import redCarImg from "../../assets/images/mypage-redcar.svg";
 import blueCarImg from "../../assets/images/mypage-bluecar.svg";
 import greenCarImg from "../../assets/images/mypage-greencar.svg";
 
-// 자동차 정보판 이미지 (있다면 사용)
 import infoBoxImg from "../../assets/images/mypage-carinfobox.svg";
 
 export default function MyPage() {
   const [selectedCar, setSelectedCar] = useState("red");
+  const [coin, setCoin] = useState(350);
 
-  const cars = {
+  const [cars, setCars] = useState({
     red: {
+      id: "red",
       name: "Red Car",
       speed: 10,
       img: redCarImg,
       locked: false,
     },
     blue: {
+      id: "blue",   
       name: "Blue Car",
       speed: 12,
       img: blueCarImg,
       locked: true,
     },
     green: {
+      id: "green",  
       name: "Green Car",
       speed: 14,
       img: greenCarImg,
       locked: true,
     },
+  });
+
+  const handleCarSelect = (id) => {
+    const car = cars[id];
+
+    if (car.locked) {
+      if (coin < 5) {
+        alert("코인이 부족합니다!");
+        return;
+      }
+
+      const buy = window.confirm(`${car.name}을(를) 5코인으로 구매하시겠습니까?`);
+      if (!buy) return;
+
+      setCoin((prev) => prev - 5);
+      setCars((prev) => ({
+        ...prev,
+        [id]: { ...prev[id], locked: false },
+      }));
+    }
+
+    setSelectedCar(id);
   };
 
   return (
@@ -60,17 +84,21 @@ export default function MyPage() {
           overflow: "hidden",
         }}
       >
-        <CommonHeader userName="홍길동" coin={350} />
+        <CommonHeader userName="유저_아이디" coin={coin} />
 
         <MyPageBackground />
 
         <MyPageCarList
           cars={cars}
           selectedCar={selectedCar}
-          onSelect={setSelectedCar}
+          onSelect={handleCarSelect}
         />
 
-        <MyPageCarDisplay carImage={cars[selectedCar].img} />
+        <MyPageCarDisplay 
+  carId={selectedCar}  
+  carImage={cars[selectedCar].img}
+/>
+
 
         <MyPageCarInfoBoard 
           car={cars[selectedCar]} 
