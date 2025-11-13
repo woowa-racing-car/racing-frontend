1316598c6d46cc3cc92b67b3c17aba109044d880
Yooaknow <dbdkfma1228@naver.com>
2025-11-14 00:18:44 +0900

    feat: 마이페이지 자동차 데이터 구조 수정

--- src/data/cars.js
+++ src/data/cars.js
@@ -3,26 +3,26 @@ import blueCar from "../assets/images/car-blue.png";
 import greenCar from "../assets/images/car-green.png";
 import lockIcon from "../assets/images/lock.png";
 
-export const cars = [
-  {
+export const cars = {
+  red: {
     id: "red",
     name: "RED car",
     speed: 10,
     image: redCar,
     locked: false,
   },
-  {
+  blue: {
     id: "blue",
     name: "BLUE car",
     speed: 12,
     image: blueCar,
     locked: true,
   },
-  {
+  green: {
     id: "green",
     name: "GREEN car",
     speed: 14,
     image: greenCar,
     locked: true,
   },
-];
+};
