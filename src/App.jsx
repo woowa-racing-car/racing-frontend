4471508a4ff4fa432f9ead7b5ca7d071e5743d22
Yooaknow <dbdkfma1228@naver.com>
2025-11-14 02:08:46 +0900

    chore: 전역 구조 및 Root 설정 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import MainPage from "./pages/MainPage";
 import MyPage from "./pages/mypage/MyPage";
 
