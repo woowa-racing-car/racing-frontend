48012ffd5b85827c2caf54ebf94ea013fc075d2c
Yooaknow <dbdkfma1228@naver.com>
2025-11-16 01:50:38 +0900

    style: 가격 입장 페이지 버튼 레이아웃 수정

--- src/pages/price-entry/PriceEntryButtons.jsx
+++ src/pages/price-entry/PriceEntryButtons.jsx
@@ -5,10 +5,21 @@ import btn1000 from "../../assets/images/price-entry-1000button.svg";
 
 const PriceEntryButtons = () => {
   return (
-    <div className="entry-button-area">
-      <img src={btn100} alt="100원 입장" className="entry-btn-img" />
-      <img src={btn500} alt="500원 입장" className="entry-btn-img" />
-      <img src={btn1000} alt="1000원 입장" className="entry-btn-img" />
+    <div
+      style={{
+        position: "absolute",
+        top: "50%",
+        right: "80px",
+        transform: "translateY(-50%)",
+        display: "flex",
+        flexDirection: "column",
+        gap: "5px",
+        zIndex: 15,     
+      }}
+    >
+      <img src={btn100} alt="100원 입장" style={{ width: "350px", cursor: "pointer" }} />
+      <img src={btn500} alt="500원 입장" style={{ width: "350px", cursor: "pointer" }} />
+      <img src={btn1000} alt="1000원 입장" style={{ width: "350px", cursor: "pointer" }} />
     </div>
   );
 };
