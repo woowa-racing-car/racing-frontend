5129efa4308bf013c1e8be107e8756833c094042
Yooaknow <dbdkfma1228@naver.com>
2025-11-18 00:33:50 +0900

    chore: RoomList 기능 추가에 따른 App 라우팅 수정

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import MainPage from "./pages/MainPage";
 import MyPage from "./pages/mypage/MyPage";
 import StartPage from "./pages/start/StartPage";
 import PriceEntry from "./pages/price-entry/PriceEntry";
+import RoomList from "./pages/room-list/RoomList";
 
 
 
@@ -17,6 +18,7 @@ export default function App() {
         <Route path="/mypage" element={<MyPage />} />
         <Route path="/start" element={<StartPage />} />
         <Route path="/price-entry" element={<PriceEntry />} />
+        <Route path="/rooms/:price" element={<RoomList />} />
       </Routes>
     </Router>
   );
