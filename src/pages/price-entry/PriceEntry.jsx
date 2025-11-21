c021e6c428752695144ce3694ea060e59622fd03
JJhwan7099 <ghks7099@naver.com>
2025-11-22 04:05:15 +0900

    refactor: 패키지 구조 변경

--- src/pages/price-entry/PriceEntry.jsx
+++ src/pages/price-entry/PriceEntry.jsx
@@ -1,10 +1,17 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PriceEntryBackground from "./PriceEntryBackground";
 import PriceEntryButtons from "./PriceEntryButtons";
 import CommonHeader from "../../components/CommonHeader";
+import { connectStomp, getClient } from "../../stomp/StompClient";
 
 const PriceEntry = () => {
   const [coin, setCoin] = useState(350);
+
+
+  // useEffect(() => {
+  //   const client = getClient();
+  //   client.deactivate();
+  // });
 
   return (
     <div
