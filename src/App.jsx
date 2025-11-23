2f620fa366a21417b8faa5cc55d261281a2cede9
Yooaknow <dbdkfma1228@naver.com>
2025-11-23 12:10:07 +0900

    feat: 결과 페이지 라우트 추가 및 연결

--- src/App.jsx
+++ src/App.jsx
@@ -10,25 +10,26 @@ import PriceEntry from "./pages/price-entry/PriceEntry";
 import RoomList from "./pages/room-list/RoomList";
 import StartTransition from "./pages/start/StartTransition";
 import IntroPage from "./pages/main/IntroPage";
-
-
 import RacePage from "./pages/race/RacePage";
+
+import GameResultPage from "./pages/result/GameResultPage";
 
 export default function App() {
   return (
     <UserProvider>
       <Router>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/start" element={<StartPage />} />
-        <Route path="/price-entry" element={<PriceEntry />} />
-        <Route path="/rooms/:price" element={<RoomList />} />
-        <Route path="/race/:price" element={<RacePage />} />
-        <Route path="/start-transition" element={<StartTransition />} />
-        <Route path="/intro" element={<IntroPage />} />
-      </Routes>
-    </Router>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/start" element={<StartPage />} />
+          <Route path="/price-entry" element={<PriceEntry />} />
+          <Route path="/rooms/:price" element={<RoomList />} />
+          <Route path="/race/:price" element={<RacePage />} />
+          <Route path="/start-transition" element={<StartTransition />} />
+          <Route path="/intro" element={<IntroPage />} />
+          <Route path="/result" element={<GameResultPage />} />
+        </Routes>
+      </Router>
     </UserProvider>
   );
 }
